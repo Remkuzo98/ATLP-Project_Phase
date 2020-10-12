dc76d98131a55618f644a2fcc2b05371eb80689b
nellyganza <nishimwelys@gmail.com>
2020-10-12 18:16:49 +0200

    fixing eslint

---
+++ README.md
@@ -0,0 +1,1 @@
+# ATLP-Project_Phase
---
+++ src/contollers/index.js
@@ -0,0 +1,3 @@
+exports.getWelcomeController=(req,res)=>{
+	res.status(200).json({message:'Well come to Barefoot Nomad global travel and accommodation easy'});
+};
