852cb3673ea22b5f6ae040490790bc58e9aac4e2
UMuhire heritier <muhire416@gmail.com>
2020-10-12 18:59:43 +0200

    re-configure eslint

--- src/contollers/index.js
+++ src/contollers/index.js
@@ -1,3 +1,3 @@
-exports.getWelcomeController=(req,res)=>{
-	res.status(200).json({message:'Well come to Barefoot Nomad global travel and accommodation easy'});
+exports.getWelcomeController = (req, res) => {
+  res.status(200).json({ message: 'Well come to Barefoot Nomad global travel and accommodation easy' });
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import app from './app';
-const port= process.env.PORT || 5000;
 
-//listerning the server 
+const port = process.env.PORT || 5000;
 
-app.listen(port, err => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(`Server is listerning on ${port}`);
-	}
+// listerning the server
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server is listerning on ${port}`);
+  }
 });
